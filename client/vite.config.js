72821b8109e3dc84e94b3fb9244d9c65c92dfeec
siva <smuppidi@cscinfo.com>
2024-04-11 20:32:21 +0530

    configs added

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
+  build:{
+    outDir:"dist"
+  }
 });
